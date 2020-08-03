9159c472c88e1621850eb32154b056a982460981
Allan Cheng <allanhfcheng@gmail.com>
2020-08-02 23:35:15 -0700

    date month fix

--- src/ClassDetails/ClassDetails.js
+++ src/ClassDetails/ClassDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ApiContext from '../ApiContext/ApiContext';
 import { NavLink, Link } from 'react-router-dom';
-import HomeworkList from '../HomeworkList/HomeworkList'
+import './ClassDetails.css';
 
 
 export default class ClassDetails extends React.Component {
@@ -31,13 +31,13 @@ export default class ClassDetails extends React.Component {
 <button type="cancel" className="cancelShowButton" onClick={()=>this.cancelHandle()}>
             Close Details
         </button>
-    { <h3> {schoolClass.classname}</h3>}
-  { <h3> Started {schoolClass.startdate}</h3>}
-  { <h3> Ends {schoolClass.finishdate}</h3>}
-    { <h3> Location {schoolClass.building}</h3>}  
-    { <h3> Teacher {schoolClass.teacher}</h3>}  
-    { <h3> Class Start Time {schoolClass.starttime}</h3>}  
-    { <h3> Class End Time {schoolClass.endtime}</h3>}  
+    { <div className="schoolClassName"> {schoolClass.classname}</div>}
+  { <div className="startDate"> Started: {schoolClass.startdate}</div>}
+  { <div className="endDate"> Ends: {schoolClass.finishdate}</div>}
+    { <div className="location"> Location: {schoolClass.building}</div>}  
+    { <div className="teacher"> Teacher: {schoolClass.teacher}</div>}  
+    { <div className="startTime"> Class Start Time: {schoolClass.starttime}</div>}  
+    { <div className="endTime"> Class End Time: {schoolClass.endtime}</div>}  
     
     <NavLink className="addClassPath"
             to={`/ClassDetails/Edit/${this.context.getClassId()}`}
