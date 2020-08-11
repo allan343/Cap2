4b50513f4c6586ce384edc8fe854548a45a6670f
Allan Cheng <allanhfcheng@gmail.com>
2020-08-10 23:28:00 -0700

    adding tests

--- src/HomeworkItem/HomeworkItem.test.js
+++ src/HomeworkItem/HomeworkItem.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import HomeworkItem from './HomeworkItem';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
-      <App />
+     <HomeworkItem/>
     </BrowserRouter>,
     div
   )
