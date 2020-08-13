a624ee1a48f09d5c837835d11be194e98852ed9c
Allan Cheng <allanhfcheng@gmail.com>
2020-08-12 23:04:28 -0700

    comments

--- src/AddHomeworkForm/AddHomeworkForm.js
+++ src/AddHomeworkForm/AddHomeworkForm.js
@@ -6,10 +6,10 @@ class AddHomeworkForm extends React.Component {
   static contextType = ApiContext;
   // static contextType = ApiContext;
   // component that allows user
-  // to fill in show details
+  // to fill in homework details
   static defaultProps = {
     homeworkid: '',
-    classid:'',
+    classid: '',
     homeworkdescription: '',
     schoolclass: '',
     homeworktype: '',
@@ -19,7 +19,7 @@ class AddHomeworkForm extends React.Component {
   };
 
   constructor(props) {
-    //states a show can have
+    //states a homework can have
     super(props);
     this.state = {
       homeworkid: '',
@@ -31,60 +31,47 @@ class AddHomeworkForm extends React.Component {
       schoolclass: "",
       homeworktype: "Homework",
       duedate: "",
-      duetime:"1:00am",
+      duetime: "1:00am",
       homeworkpriority: "High"
     };
 
-   // this.updateClass = this.updateClass.bind(this);
   }
-  //methods to update show state from user input
+  //methods to update homework state from user input
   cancelHandle = (e) => {
     e.preventDefault();
-   // this.props.history.push(`/`);
-   this.props.history.goBack();
+    this.props.history.goBack();
   }
 
+  //update homework description
   updateDescription(homeworkdescription) {
-
     this.setState({ homeworkdescription: { value: homeworkdescription, touched: true } });
-
   }
- 
-  updateClass =(event)=> {
-    console.log(event.target.value);
-    this.setState({classid:event.target.value})
+
+  //update class
+  //a homework belongs to a school class
+  //user can update this field
+  updateClass = (event) => {
+    this.setState({ classid: event.target.value })
     this.setState({ schoolclass: this.context.getClass(event.target.value).classname });
   };
-  updateType =(event)=> {
-    console.log(event.target.value);
+
+  //update homework type: reading, homework, quiz etc.
+  updateType = (event) => {
     this.setState({ homeworktype: event.target.value });
   };
 
-  updatePriority =(event)=> {
-    console.log(event.target.value);
+  //update priority: high medium low etc.
+  updatePriority = (event) => {
     this.setState({ homeworkpriority: event.target.value });
   };
 
-  updateDueDate(date) {
-    console.log(date);
-    var d = new Date(date);
-    var n = d.getDay();
-    console.log(n);
-    let dueDate = date;
-    console.log(dueDate);
+  updateDueDate(dueDate) {
     this.setState({ duedate: dueDate });
   }
 
-  updateDueTime =(event)=> {
-
+  updateDueTime = (event) => {
     this.setState({ duetime: event.target.value });
   };
-
-  updatePriority(priority) {
-    this.setState({ homeworkpriority: priority });
-  }
-
-
 
   render() {
     let classList = this.context.schoolClasses;
@@ -93,7 +80,6 @@ class AddHomeworkForm extends React.Component {
         <form className="folder" onSubmit={(event) => {
           event.preventDefault();
           let homework = {
-            //homeworkid: Math.random(),
             classid: this.state.classid,
             homeworkdescription: this.state.homeworkdescription.value,
             schoolclass: this.state.schoolclass,
@@ -101,107 +87,99 @@ class AddHomeworkForm extends React.Component {
             duedate: this.state.duedate,
             duetime: this.state.duetime,
             homeworkpriority: this.state.homeworkpriority
-
           }
-          if(homework.classid==""||homework.schoolclass=="")
-          {
-            console.log("here");
-            homework.classid= this.context.getFirstClassId();
-            homework.schoolclass=this.context.getFirstClassName();
+          //the user has to create a class before he can add a homework
+          //if the user only created one class
+          //a homework created will belong to that one class (ie. user created class math  homework belongs to math)
+          if (homework.classid === "" || homework.schoolclass === "") {
+            homework.classid = this.context.getFirstClassId();
+            homework.schoolclass = this.context.getFirstClassName();
           }
+          //add homework created to homework array 
+          //navigate back to previous page.
           this.context.addHomework(homework);
-
-        //  this.props.history.push(`/`);
-        this.props.history.goBack();
-          console.log("Homework" + this.context.homeworkList);
+          this.props.history.goBack();
+          
         }}>
           <div className="homework-details">
-          <h2 className="homeworkDetailsHeading"> Homework Details</h2>
-          <button type="cancel" className="cancelShowButton" onClick={this.cancelHandle}>
-            Back
+            <h2 className="homeworkDetailsHeading"> Homework Details</h2>
+            <button type="cancel" className="cancelShowButton" onClick={this.cancelHandle}>
+              Back
         </button>
-       
-          <div className="form-group">
-            <label htmlFor="homeworkDesc">Description *</label>
-            <input type="text" className="folder__control"
-              name="classname" id="classname" value={this.state.homeworkdescription.value} onChange={e => this.updateDescription(e.target.value)} required="required"/>
-            <br/>
-            <label htmlFor="cars">Class:</label>
-            <select  className="schoolclass" name="schoolclass" id="schoolclass" value={this.state.classid} onChange={this.updateClass} required="required">
-              {
-            
-                classList.map(schoolClass =>
-
-                  <option  value={schoolClass.id}>{schoolClass.classname}</option>
-                )
-              }
-            </select>
-            <br/>
-            
-            <label htmlFor="classType">Type:</label>
-            <select className="classType" name="classType" id="classType" onChange={this.updateType}>
-              <option defaultValue value="Homework">Homework</option>
-              <option value="Test">Test</option>
-              <option value="Study">Study</option>
-              <option value="Read">Read</option>
-              <option value="Paper">Paper</option>
-              <option value="Presentation">Presentation</option>
-              <option value="Lab">Lab</option>
-              <option value="Final">Final</option>
-              <option value="Midterm">Midterm</option>
-              <option value="Quiz">Quiz</option>
-              <option value="Project">Project</option>
-            </select>
-           
-            <br/>
-            <label htmlFor="dueDate">Due Date</label>
-            <input type="date" id="dueDate" name="dueDate" onChange={e => this.updateDueDate(e.target.value)} required="required"></input>
-
-            <br/>
-            <label htmlFor="name">Due Time</label>
-            <select name="starttime" id="starttime" onChange={this.updateDueTime} required="required">
-              <option defaultValue value="1:00am">1:00am</option>
-              <option value="2:00am">2:00am</option>
-              <option value="3:00am">3:00am</option>
-              <option value="4:00am">4:00am</option>
-              <option value="5:00am">5:00am</option>
-              <option value="6:00am">6:00am</option>
-              <option value="7:00am">7:00am</option>
-              <option value="8:00am">8:00am</option>
-              <option value="9:00am">9:00am</option>
-              <option value="10:00am">10:00am</option>
-              <option value="11:00am">11:00am</option>
-              <option value="12:00pm">12:00pm</option>
-              <option value="1:00pm">1:00pm</option>
-              <option value="2:00pm">2:00pm</option>
-              <option value="3:00pm">3:00pm</option>
-              <option value="4:00pm">4:00pm</option>
-              <option value="5:00pm">5:00pm</option>
-              <option value="6:00pm">6:00pm</option>
-              <option value="7:00pm">7:00pm</option>
-              <option value="8:00pm">8:00pm</option>
-              <option value="9:00pm">9:00pm</option>
-              <option value="10:00pm">10:00pm</option>
-              <option value="11:00pm">11:00pm</option>
-              <option value="12:00am">12:00am</option>
-            </select>
-            <br/>
-            <br/>
-            <label htmlFor="priority">Priority</label>
-            <select name="priority" id="priority" onChange={this.updatePriority} >
-              <option value="High"  defaultValue>High</option>
-              <option value="Medium">Medium</option>
-              <option value="Low">Low</option>
-            </select>
-            <br/>
-            
-          </div>
-          <div className="addHomework__button__group">
-
-            <button type="submit" className="saveHomeworkButton">
-              Save
+            <div className="form-group">
+              <label htmlFor="homeworkDesc">Description *</label>
+              <input type="text" className="folder__control"
+                name="classname" id="classname" value={this.state.homeworkdescription.value} onChange={e => this.updateDescription(e.target.value)} required="required" />
+              <br />
+              <label htmlFor="class">Class:</label>
+              <select className="schoolclass" name="schoolclass" id="schoolclass" value={this.state.classid} onChange={this.updateClass} required="required">
+                {
+                  classList.map(schoolClass =>
+                    <option value={schoolClass.id}>{schoolClass.classname}</option>
+                  )
+                }
+              </select>
+              <br />
+              <label htmlFor="classType">Type:</label>
+              <select className="classType" name="classType" id="classType" onChange={this.updateType}>
+                <option defaultValue value="Homework">Homework</option>
+                <option value="Test">Test</option>
+                <option value="Study">Study</option>
+                <option value="Read">Read</option>
+                <option value="Paper">Paper</option>
+                <option value="Presentation">Presentation</option>
+                <option value="Lab">Lab</option>
+                <option value="Final">Final</option>
+                <option value="Midterm">Midterm</option>
+                <option value="Quiz">Quiz</option>
+                <option value="Project">Project</option>
+              </select>
+              <br />
+              <label htmlFor="dueDate">Due Date</label>
+              <input type="date" id="dueDate" name="dueDate" onChange={e => this.updateDueDate(e.target.value)} required="required"></input>
+              <br />
+              <label htmlFor="name">Due Time</label>
+              <select name="starttime" id="starttime" onChange={this.updateDueTime} required="required">
+                <option defaultValue value="1:00am">1:00am</option>
+                <option value="2:00am">2:00am</option>
+                <option value="3:00am">3:00am</option>
+                <option value="4:00am">4:00am</option>
+                <option value="5:00am">5:00am</option>
+                <option value="6:00am">6:00am</option>
+                <option value="7:00am">7:00am</option>
+                <option value="8:00am">8:00am</option>
+                <option value="9:00am">9:00am</option>
+                <option value="10:00am">10:00am</option>
+                <option value="11:00am">11:00am</option>
+                <option value="12:00pm">12:00pm</option>
+                <option value="1:00pm">1:00pm</option>
+                <option value="2:00pm">2:00pm</option>
+                <option value="3:00pm">3:00pm</option>
+                <option value="4:00pm">4:00pm</option>
+                <option value="5:00pm">5:00pm</option>
+                <option value="6:00pm">6:00pm</option>
+                <option value="7:00pm">7:00pm</option>
+                <option value="8:00pm">8:00pm</option>
+                <option value="9:00pm">9:00pm</option>
+                <option value="10:00pm">10:00pm</option>
+                <option value="11:00pm">11:00pm</option>
+                <option value="12:00am">12:00am</option>
+              </select>
+              <br />
+              <br />
+              <label htmlFor="priority">Priority</label>
+              <select name="priority" id="priority" onChange={this.updatePriority} >
+                <option value="High" defaultValue>High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+              </select>
+              <br />
+            </div>
+            <div className="addHomework__button__group">
+              <button type="submit" className="saveHomeworkButton">
+                Save
         </button>
-          </div>
+            </div>
           </div>
         </form>
       </div>
