d68192b8135ea657d7632d858cbe21458b43d8e8
Allan Cheng <allanhfcheng@gmail.com>
2020-08-13 00:21:10 -0700

    key problem fix attempt

--- src/Homework/Homework.js
+++ src/Homework/Homework.js
@@ -3,15 +3,13 @@ import React from 'react';
 import HomeworkList from '../HomeworkList/HomeworkList';
 import ApiContext from '../ApiContext/ApiContext';
 import HeaderNav from '../HeaderNav/HeaderNav';
-//import './Homework.css'
 import '../main.css';
 
 export default class Home extends React.Component {
   static contextType = ApiContext;
 
-   //filter shows by their not yet watching state
-  //only show shows that have that state for this view
   render() {
+    // get the present date
     var Now = Date.now();
     let pendingMessage="Nothing upcoming.";
     let dueMessage ="Nothing is late."
@@ -20,10 +18,9 @@ export default class Home extends React.Component {
        
        <HeaderNav></HeaderNav>
        <div className="columns">
-      
-
+      {/*filter by homework that has due date before now date, hence it is late*/}
       <HomeworkList message ={dueMessage} heading="Late" homeworkList={this.context.homeworkList.filter(homework =>new Date(homework.duedate) < Now)} ></HomeworkList>
-
+          {/*filter by homework that has due date after now date, hence it is not late*/}
        <HomeworkList  message= {pendingMessage}heading = "Upcoming" homeworkList={this.context.homeworkList.filter(homework => new Date(homework.duedate) > Now)} ></HomeworkList>
     
       </div>
