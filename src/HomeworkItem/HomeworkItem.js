de80b773b14648fae68fc926f7e56fd17d940d36
Allan Cheng <allanhfcheng@gmail.com>
2020-08-15 18:46:32 -0700

    styling

--- src/HomeworkItem/HomeworkItem.js
+++ src/HomeworkItem/HomeworkItem.js
@@ -8,7 +8,7 @@ export default function HomeworkItem(props) {
     <div className='HomeworkItem__row'>
       {<h4 className="homeworkDescription"> {props.homeworkdescription}</h4>}
       {<div className="schoolClass"><span id="item-details-label"> Class: </span>{props.schoolclass}</div>}
-      {<div className="dueDate"><span id="item-details-label"> Due Date: </span>{context.formatDate(props.duedate)}</div>}
+      {<div className="dueDate"><span id="item-details-label"> Due Date: </span><span id="duedatetext">{context.formatDate(props.duedate)}</span></div>}
       {<div className="dueTime"><span id="item-details-label"> Due time: </span> {props.duetime}</div>}
     </div>
   )
